b727b7496768a44f3f42904efb8b1858a814f33f
Rodrigo Willrich <rodrigo.willrich@hotmail.com>
2022-08-05 19:57:44 -0300

    Implement basic editor with draft-js

--- src/modules/editor/components/Editor.tsx
+++ src/modules/editor/components/Editor.tsx
@@ -1,7 +1,26 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useEffect, useState } from 'react'
+import { Editor as DraftJsEditor, EditorState } from 'draft-js'
 
 export type EditorProps = {}
 
+const useIsMounted = () => {
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  return isMounted
+}
+
 export const Editor: FunctionComponent<EditorProps> = () => {
-  return <textarea />
+  const isMounted = useIsMounted()
+
+  const [editorState, setEditorState] = useState<EditorState>(() =>
+    EditorState.createEmpty()
+  )
+
+  if (!isMounted) return null
+
+  return <DraftJsEditor editorState={editorState} onChange={setEditorState} />
 }
